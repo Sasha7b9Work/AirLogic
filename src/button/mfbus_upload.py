7525a76065c53d4e6d3f74b49cab711bfdd94c87
Sasha7b9Work <Phuntik-it@tut.by>
2024-08-01 12:47:22 +0300

    detrash

--- src/button/mfbus_upload.py
+++
@@ -1,6 +0,0 @@
-Import("env")
-
-env.Replace(
-    UPLOADER="./mfupload",
-    UPLOADCMD="echo ================================================== && $UPLOADER $UPLOADERFLAGS 'alpro_v10_button,$BUILD_DIR/${PROGNAME}.bin'"
-)
--- src/mf_lcd/mfbus_upload.py
+++
@@ -1,6 +0,0 @@
-Import("env")
-
-env.Replace(
-    UPLOADER="./mfupload",
-    UPLOADCMD="echo ================================================== && $UPLOADER $UPLOADERFLAGS 'alpro_v10_lcd,$BUILD_DIR/${PROGNAME}.bin'"
-)
