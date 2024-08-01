5681356b5c68433da3bc1b46c34c11cfff0f25fc
Sasha7b9Work <Phuntik-it@tut.by>
2024-08-01 10:13:12 +0300

    *

--- Readme.md
+++ Readme.md
@@ -6,7 +6,6 @@
 
 1. Устанавливаем [Platformio](https://platformio.org/) сборочную среду
     ``` bash
-    python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
     sudo mkdir -p /usr/local/bin
     sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
     sudo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
---
+++ install.sh
@@ -0,0 +1,7 @@
+wget -O get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
+python3 get-platformio.py
+
+sudo mkdir -p /usr/local/bin
+sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
+sudo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
+sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
