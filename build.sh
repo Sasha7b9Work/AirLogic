2945deb113c5eb25186228de0f0e1b1d7d60a311
Sasha7b9Work <Phuntik-it@tut.by>
2024-08-02 13:51:58 +0300

    refact

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ pio run
 cp ./.pio/build/ALPRO_button/firmware.bin ~/airlogic/out/button.bin
 cp ./.pio/build/ALPRO_button_v2/firmware.bin ~/airlogic/out/button_v2.bin
 
-cd ~/airlogic/src2/bootloader
+cd ~/airlogic/src2/boot
 pio run
-cp ./.pio/build/mfBoot/firmware.bin ~/airlogic/out/boot.bin
+cp ./.pio/build/boot/firmware.bin ~/airlogic/out/boot.bin
 
 cd ~/airlogic/src2/bootloader_f401
 pio run
