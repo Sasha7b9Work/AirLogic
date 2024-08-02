fdb52cfbb48a4a5b362212850c908eb1c59c89e7
Sasha7b9Work <Phuntik-it@tut.by>
2024-08-02 14:46:01 +0300

    refact

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 pio pkg install -g --platform "platformio/ststm32@^15.4.1"
 
 # Копируем собственную реализацию [Arduino_Core_STM32](https://github.com/stm32duino/Arduino_Core_STM32) (коммит fe500808f773c00f537ba3242ed18e0bdc5008f9)
-cp -r ~/airlogic/src/framework-ststm32 ~/.platformio/packages/
+cp -r ~/airlogic/new_files/framework-ststm32 ~/.platformio/packages/
 
 cp ~/airlogic/new_files/platforms/ststm32/platform.json ~/.platformio/platforms/ststm32
 cp ~/airlogic/new_files/platforms/ststm32/builder/frameworks/stm32.py ~/.platformio/platforms/ststm32/builder/frameworks
