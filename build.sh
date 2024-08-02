ae7f2c2e607accf98a2d6c4940bd268211f3c431
Sasha7b9Work <Phuntik-it@tut.by>
2024-08-02 14:36:27 +0300

    refact

--- build.sh
+++ build.sh
@@ -18,15 +18,15 @@ cd ~/airlogic/src2/boot
 pio run
 cp ./.pio/build/boot/firmware.bin ~/airlogic/out/boot.bin
 
-cd ~/airlogic/src2/bootloader_f401
+cd ~/airlogic/src2/boot_lcd
 pio run
-cp ./.pio/build/lcd_boot/firmware.bin ~/airlogic/out/boot_lcd.bin
-cp ./.pio/build/lcd_boot_ext/firmware.bin ~/airlogic/out/boot_lcd_ext.bin
+cp ./.pio/build/boot_lcd/firmware.bin ~/airlogic/out/boot_lcd.bin
+cp ./.pio/build/boot_lcd_ext/firmware.bin ~/airlogic/out/boot_lcd_ext.bin
 
-cd ~/airlogic/src2/bootloader_f051
+cd ~/airlogic/src2/boot_button
 pio run
-cp ./.pio/build/button_boot_rev1/firmware.bin ~/airlogic/out/boot_button.bin
-cp ./.pio/build/button_boot_rev2/firmware.bin ~/airlogic/out/boot_button_v2.bin
+cp ./.pio/build/boot_button/firmware.bin ~/airlogic/out/boot_button.bin
+cp ./.pio/build/boot_button_v2/firmware.bin ~/airlogic/out/boot_button_v2.bin
 
 cd ~/airlogic/src2/flasher
 pio run
